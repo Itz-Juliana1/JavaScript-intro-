e01eaa8daf3775bdf16e39cc9e234a2e08d2c662
Itz-Juliana1 <julezdik@gmail.com>
2026-02-14 14:57:45 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,6 @@
+const Name = "Juliana";
+const age = "17";
+const favorite_programming_language = "HTML";
+console.log ("Name:", Name);
+console.log ("Age:", age);
+console.log ("My Favourite Programming Language:", favorite_programming_language);
